3c84a0de8b178b351f7fe8f4a3ff734023ef2081
Alwaz <alwazkazi3@gmail.com>
2025-01-10 19:23:40 +0500

    chore(shad-cn): init shadcn and setup custom theme

--- server/src/app.ts
+++ server/src/app.ts
@@ -8,6 +8,12 @@ import mongoose from "mongoose";
 import morgan from "morgan";
 
 const app = express();
+
+mongoose
+  .connect(process.env.MONGODB_URI!)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error(err));
+
 app.use(cors());
 app.use(helmet());
 app.use(morgan("dev"));
